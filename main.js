f23c8337bb1abba9caf0b9a8e4dc24ee9da03751
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-09-07 20:54:04 +0530

    error handling and getting HTML

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+const { crawlPage } = require("./crawl");
+function main() {
+  if (process.argv.length < 3) {
+    console.log("no website provided");
+    process.exit(1);
+  } else if (process.argv.length > 3) {
+    console.log("too many command line args");
+  }
+
+  const baseURL = process.argv[2];
+
+  console.log(`Starting crawl of ${baseURL}`);
+  crawlPage(baseURL);
+}
+
+main();
